21a25ead2ad16a77bb3776644ed578fce6b24fe7
aidanmocke <31958238+aidanmocke@users.noreply.github.com>
2018-11-16 22:38:33 +0900

    Create PIC32MZ_9.c
    Pulse Width Modulation (PWM) and servos on the PIC32MZ

---
+++ PIC32MZ_9.c
@@ -0,0 +1,193 @@
+// PIC32MZ_9.c - Pulse Width Modulation (PWM) and servos on the PIC32MZ
+// by Aidan Mocke 2018-11-16
+//
+
+// PIC32MZ2048EFH144 Configuration Bit Settings
+
+// DEVCFG3
+// USERID = No Setting
+#pragma config FMIIEN = ON              // Ethernet RMII/MII Enable (MII Enabled)
+#pragma config FETHIO = ON              // Ethernet I/O Pin Select (Default Ethernet I/O)
+#pragma config PGL1WAY = OFF            // Permission Group Lock One Way Configuration (Allow multiple reconfigurations)
+#pragma config PMDL1WAY = OFF           // Peripheral Module Disable Configuration (Allow multiple reconfigurations)
+#pragma config IOL1WAY = OFF            // Peripheral Pin Select Configuration (Allow multiple reconfigurations)
+#pragma config FUSBIDIO = OFF           // USB USBID Selection (Controlled by Port Function)
+
+// DEVCFG2
+#pragma config FPLLIDIV = DIV_1         // System PLL Input Divider (1x Divider)
+#pragma config FPLLRNG = RANGE_5_10_MHZ // System PLL Input Range (5-10 MHz Input)
+#pragma config FPLLICLK = PLL_FRC       // System PLL Input Clock Selection (POSC is input to the System PLL)
+#pragma config FPLLMULT = MUL_50        // System PLL Multiplier (PLL Multiply by 50)
+#pragma config FPLLODIV = DIV_2         // System PLL Output Clock Divider (2x Divider)
+#pragma config UPLLFSEL = FREQ_24MHZ    // USB PLL Input Frequency Selection (USB PLL input is 24 MHz)
+
+// DEVCFG1
+#pragma config FNOSC = SPLL             // Oscillator Selection Bits (System PLL)
+#pragma config DMTINTV = WIN_127_128    // DMT Count Window Interval (Window/Interval value is 127/128 counter value)
+#pragma config FSOSCEN = OFF            // Secondary Oscillator Enable (Disable SOSC)
+#pragma config IESO = OFF               // Internal/External Switch Over (Disabled)
+#pragma config POSCMOD = OFF            // Primary Oscillator Configuration (Disabled)
+#pragma config OSCIOFNC = OFF           // CLKO Output Signal Active on the OSCO Pin (Disabled)
+#pragma config FCKSM = CSECME           // Clock Switching and Monitor Selection (Clock Switch Enabled, FSCM Enabled)
+#pragma config WDTPS = PS1048576        // Watchdog Timer Postscaler (1:1048576)
+#pragma config WDTSPGM = STOP           // Watchdog Timer Stop During Flash Programming (WDT stops during Flash programming)
+#pragma config WINDIS = NORMAL          // Watchdog Timer Window Mode (Watchdog Timer is in non-Window mode)
+#pragma config FWDTEN = OFF             // Watchdog Timer Enable (WDT Disabled)
+#pragma config FWDTWINSZ = WINSZ_25     // Watchdog Timer Window Size (Window size is 25%)
+#pragma config DMTCNT = DMT31           // Deadman Timer Count Selection (2^31 (2147483648))
+#pragma config FDMTEN = OFF             // Deadman Timer Enable (Deadman Timer is disabled)
+
+// DEVCFG0
+#pragma config DEBUG = OFF              // Background Debugger Enable (Debugger is disabled)
+#pragma config JTAGEN = OFF             // JTAG Enable (JTAG Disabled)
+#pragma config ICESEL = ICS_PGx1        // ICE/ICD Comm Channel Select (Communicate on PGEC1/PGED1)
+#pragma config TRCEN = OFF              // Trace Enable (Trace features in the CPU are disabled)
+#pragma config BOOTISA = MIPS32         // Boot ISA Selection (Boot code and Exception code is MIPS32)
+#pragma config FECCCON = OFF_UNLOCKED   // Dynamic Flash ECC Configuration (ECC and Dynamic ECC are disabled (ECCCON bits are writable))
+#pragma config FSLEEP = OFF             // Flash Sleep Mode (Flash is powered down when the device is in Sleep mode)
+#pragma config DBGPER = PG_ALL          // Debug Mode CPU Access Permission (Allow CPU access to all permission regions)
+#pragma config SMCLR = MCLR_NORM        // Soft Master Clear Enable bit (MCLR pin generates a normal system Reset)
+#pragma config SOSCGAIN = GAIN_2X       // Secondary Oscillator Gain Control bits (2x gain setting)
+#pragma config SOSCBOOST = ON           // Secondary Oscillator Boost Kick Start Enable bit (Boost the kick start of the oscillator)
+#pragma config POSCGAIN = GAIN_2X       // Primary Oscillator Gain Control bits (2x gain setting)
+#pragma config POSCBOOST = ON           // Primary Oscillator Boost Kick Start Enable bit (Boost the kick start of the oscillator)
+#pragma config EJTAGBEN = NORMAL        // EJTAG Boot (Normal EJTAG functionality)
+#pragma config CP = OFF                 // Code Protect (Protection Disabled)
+
+
+#include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <proc/p32mz2048efh144.h>
+
+#define SYS_FREQ 200000000              // Running at 200MHz
+
+void delay_us(unsigned int us)
+{
+    // Convert microseconds us into how many clock ticks it will take
+    us *= (SYS_FREQ / 1000000) / 2; // Core Timer updates every 2 ticks
+                      
+    _CP0_SET_COUNT(0); // Set Core Timer count to 0
+
+    while (us > _CP0_GET_COUNT()); // Wait until Core Timer count reaches the number we calculated earlier
+}
+
+void delay_ms(int ms)
+{
+    delay_us(ms * 1000);
+}
+
+void set_performance_mode()
+{   
+	unsigned int cp0;
+	
+    // Unlock Sequence
+    asm volatile("di"); // Disable all interrupts
+    SYSKEY = 0xAA996655;
+    SYSKEY = 0x556699AA;  
+
+    // PB1DIV
+    // Peripheral Bus 1 cannot be turned off, so there's no need to turn it on
+    PB1DIVbits.PBDIV = 1; // Peripheral Bus 1 Clock Divisor Control (PBCLK1 is SYSCLK divided by 2)
+
+    // PB2DIV
+    PB2DIVbits.ON = 1; // Peripheral Bus 2 Output Clock Enable (Output clock is enabled)
+    PB2DIVbits.PBDIV = 1; // Peripheral Bus 2 Clock Divisor Control (PBCLK2 is SYSCLK divided by 2)
+
+    // PB3DIV
+    PB3DIVbits.ON = 1; // Peripheral Bus 2 Output Clock Enable (Output clock is enabled)
+    PB3DIVbits.PBDIV = 1; // Peripheral Bus 3 Clock Divisor Control (PBCLK3 is SYSCLK divided by 2)
+
+    // PB4DIV
+    PB4DIVbits.ON = 1; // Peripheral Bus 4 Output Clock Enable (Output clock is enabled)
+    while (!PB4DIVbits.PBDIVRDY); // Wait until it is ready to write to
+    PB4DIVbits.PBDIV = 0; // Peripheral Bus 4 Clock Divisor Control (PBCLK4 is SYSCLK divided by 1)
+
+    // PB5DIV
+    PB5DIVbits.ON = 1; // Peripheral Bus 5 Output Clock Enable (Output clock is enabled)
+    PB5DIVbits.PBDIV = 1; // Peripheral Bus 5 Clock Divisor Control (PBCLK5 is SYSCLK divided by 2)
+
+    // PB7DIV
+    PB7DIVbits.ON = 1; // Peripheral Bus 7 Output Clock Enable (Output clock is enabled)
+    PB7DIVbits.PBDIV = 0; // Peripheral Bus 7 Clock Divisor Control (PBCLK7 is SYSCLK divided by 1)
+
+    // PB8DIV
+    PB8DIVbits.ON = 1; // Peripheral Bus 8 Output Clock Enable (Output clock is enabled)
+    PB8DIVbits.PBDIV = 1; // Peripheral Bus 8 Clock Divisor Control (PBCLK8 is SYSCLK divided by 2)
+
+    // PRECON - Set up prefetch
+    PRECONbits.PFMSECEN = 0; // Flash SEC Interrupt Enable (Do not generate an interrupt when the PFMSEC bit is set)
+    PRECONbits.PREFEN = 0b11; // Predictive Prefetch Enable (Enable predictive prefetch for any address)
+    PRECONbits.PFMWS = 0b010; // PFM Access Time Defined in Terms of SYSCLK Wait States (Two wait states)
+
+    // Set up caching
+    cp0 = _mfc0(16, 0);
+    cp0 &= ~0x07;
+    cp0 |= 0b011; // K0 = Cacheable, non-coherent, write-back, write allocate
+    _mtc0(16, 0, cp0);  
+
+    // Lock Sequence
+    SYSKEY = 0x33333333;
+    asm volatile("ei"); // Enable all interrupts
+}
+
+void PWM_init()
+{
+    T2CON	= 0x0;		// Disable timer 2 when setting it up
+    TMR2	= 0;		// Set timer 2 counter to 0
+    
+    // Set up the period. Period = PBCLK3 frequency, which is SYS_FREQ / 2, divided by 50 Hhz and then divided again by 32 for our chosen pre-scaler.
+    PR2 = SYS_FREQ / 2 / 50 / 32;
+	
+	// Set up the pre-scaler
+    T2CONbits.TCKPS = 0b101; // Pre-scale of 32
+	
+	// Turn on timer 2
+	T2CONbits.TON	= 1;
+    
+	OC6CON = 0;				// Turn off Output Compare module 6
+    OC6CONbits.OCTSEL = 0; 	// Interrupt source for this module is Timer 2
+    OC6CONbits.OCM = 0b110;	// Output Compare Mode (OCM) is 6, which is PWM mode
+    OC6RS = 0;				// Initially keep the signal low for the entire duration
+    OC6CONbits.ON = 1;		// Turn on Output Compare module 6 (OC6)
+}
+
+int main()
+{
+    set_performance_mode();
+
+    // Set all port pins to digital
+    ANSELA = 0;
+    ANSELB = 0;
+    ANSELC = 0;
+    ANSELD = 0;
+    ANSELE = 0;
+    ANSELF = 0;
+    ANSELG = 0;
+    ANSELH = 0;
+    ANSELJ = 0;
+    
+    // Set all port pins to outputs
+    TRISA = 0;
+    TRISB = 0;
+    TRISC = 0;
+    TRISD = 0;
+    TRISE = 0;
+    TRISF = 0;
+    TRISG = 0;
+    TRISH = 0;
+    TRISJ = 0;
+    
+    RPC1R = 0b1100; // OC6
+               
+    PWM_init();
+    
+    while (1)
+    {
+        OC6RS = 3125;		// Move servo to 1.5ms position
+        delay_ms(1000);
+        OC6RS = 6250;		// Move servo to 2ms position
+        delay_ms(1000);
+    }
+}
