c2ffda593e57a57d9bcb0ae6f6bfc0904316ee16
aidanmocke <31958238+aidanmocke@users.noreply.github.com>
2018-11-16 22:35:42 +0900

    Uploaded source files

---
+++ PIC32MZ_2.c
@@ -0,0 +1,158 @@
+// PIC32MZ_2.c - A simple PIC32MZ skeleton
+// by Aidan Mocke 2018-04-09
+//
+// PIC32MZ2048EFH144 Configuration Bit Settings
+
+// DEVCFG3
+// USERID = No Setting
+#pragma config FMIIEN = ON              // Ethernet RMII/MII Enable (MII Enabled)
+#pragma config FETHIO = ON              // Ethernet I/O Pin Select (Default Ethernet I/O)
+#pragma config PGL1WAY = OFF            // Permission Group Lock One Way Configuration (Allow multiple reconfigurations)
+#pragma config PMDL1WAY = OFF           // Peripheral Module Disable Configuration (Allow multiple reconfigurations)
+#pragma config IOL1WAY = OFF            // Peripheral Pin Select Configuration (Allow multiple reconfigurations)
+#pragma config FUSBIDIO = OFF           // USB USBID Selection (Controlled by Port Function)
+
+// DEVCFG2
+#pragma config FPLLIDIV = DIV_3         // System PLL Input Divider (3x Divider)
+#pragma config FPLLRNG = RANGE_5_10_MHZ // System PLL Input Range (5-10 MHz Input)
+#pragma config FPLLICLK = PLL_POSC      // System PLL Input Clock Selection (POSC is input to the System PLL)
+#pragma config FPLLMULT = MUL_50        // System PLL Multiplier (PLL Multiply by 50)
+#pragma config FPLLODIV = DIV_2         // System PLL Output Clock Divider (2x Divider)
+#pragma config UPLLFSEL = FREQ_24MHZ    // USB PLL Input Frequency Selection (USB PLL input is 24 MHz)
+
+// DEVCFG1
+#pragma config FNOSC = SPLL             // Oscillator Selection Bits (System PLL)
+#pragma config DMTINTV = WIN_127_128    // DMT Count Window Interval (Window/Interval value is 127/128 counter value)
+#pragma config FSOSCEN = OFF            // Secondary Oscillator Enable (Disable SOSC)
+#pragma config IESO = OFF               // Internal/External Switch Over (Disabled)
+#pragma config POSCMOD = EC             // Primary Oscillator Configuration (External clock mode)
+#pragma config OSCIOFNC = OFF           // CLKO Output Signal Active on the OSCO Pin (Disabled)
+#pragma config FCKSM = CSECME           // Clock Switching and Monitor Selection (Clock Switch Enabled, FSCM Enabled)
+#pragma config WDTPS = PS1048576        // Watchdog Timer Postscaler (1:1048576)
+#pragma config WDTSPGM = STOP           // Watchdog Timer Stop During Flash Programming (WDT stops during Flash programming)
+#pragma config WINDIS = NORMAL          // Watchdog Timer Window Mode (Watchdog Timer is in non-Window mode)
+#pragma config FWDTEN = OFF             // Watchdog Timer Enable (WDT Disabled)
+#pragma config FWDTWINSZ = WINSZ_25     // Watchdog Timer Window Size (Window size is 25%)
+#pragma config DMTCNT = DMT31           // Deadman Timer Count Selection (2^31 (2147483648))
+#pragma config FDMTEN = OFF             // Deadman Timer Enable (Deadman Timer is disabled)
+
+// DEVCFG0
+#pragma config DEBUG = OFF              // Background Debugger Enable (Debugger is disabled)
+#pragma config JTAGEN = OFF             // JTAG Enable (JTAG Disabled)
+#pragma config ICESEL = ICS_PGx1        // ICE/ICD Comm Channel Select (Communicate on PGEC1/PGED1)
+#pragma config TRCEN = OFF              // Trace Enable (Trace features in the CPU are disabled)
+#pragma config BOOTISA = MIPS32         // Boot ISA Selection (Boot code and Exception code is MIPS32)
+#pragma config FECCCON = OFF_UNLOCKED   // Dynamic Flash ECC Configuration (ECC and Dynamic ECC are disabled (ECCCON bits are writable))
+#pragma config FSLEEP = OFF             // Flash Sleep Mode (Flash is powered down when the device is in Sleep mode)
+#pragma config DBGPER = PG_ALL          // Debug Mode CPU Access Permission (Allow CPU access to all permission regions)
+#pragma config SMCLR = MCLR_NORM        // Soft Master Clear Enable bit (MCLR pin generates a normal system Reset)
+#pragma config SOSCGAIN = GAIN_2X       // Secondary Oscillator Gain Control bits (2x gain setting)
+#pragma config SOSCBOOST = OFF           // Secondary Oscillator Boost Kick Start Enable bit (Boost the kick start of the oscillator)
+#pragma config POSCGAIN = GAIN_2X       // Primary Oscillator Gain Control bits (2x gain setting)
+#pragma config POSCBOOST = OFF           // Primary Oscillator Boost Kick Start Enable bit (Boost the kick start of the oscillator)
+#pragma config EJTAGBEN = NORMAL        // EJTAG Boot (Normal EJTAG functionality)
+#pragma config CP = OFF                 // Code Protect (Protection Disabled)
+
+
+#include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <proc/p32mz2048efh144.h>
+
+
+void set_performance_mode()
+{   
+	unsigned int cp0;
+	
+    // Unlock Sequence
+    asm volatile("di"); // Disable all interrupts
+    SYSKEY = 0xAA996655;
+    SYSKEY = 0x556699AA;  
+
+    // PB1DIV
+    // Peripheral Bus 1 cannot be turned off, so there's no need to turn it on
+    PB1DIVbits.PBDIV = 1; // Peripheral Bus 1 Clock Divisor Control (PBCLK1 is SYSCLK divided by 2)
+
+    // PB2DIV
+    PB2DIVbits.ON = 1; // Peripheral Bus 2 Output Clock Enable (Output clock is enabled)
+    PB2DIVbits.PBDIV = 1; // Peripheral Bus 2 Clock Divisor Control (PBCLK2 is SYSCLK divided by 2)
+
+    // PB3DIV
+    PB3DIVbits.ON = 1; // Peripheral Bus 2 Output Clock Enable (Output clock is enabled)
+    PB3DIVbits.PBDIV = 1; // Peripheral Bus 3 Clock Divisor Control (PBCLK3 is SYSCLK divided by 2)
+
+    // PB4DIV
+    PB4DIVbits.ON = 1; // Peripheral Bus 4 Output Clock Enable (Output clock is enabled)
+    while (!PB4DIVbits.PBDIVRDY); // Wait until it is ready to write to
+    PB4DIVbits.PBDIV = 0; // Peripheral Bus 4 Clock Divisor Control (PBCLK4 is SYSCLK divided by 1)
+
+    // PB5DIV
+    PB5DIVbits.ON = 1; // Peripheral Bus 5 Output Clock Enable (Output clock is enabled)
+    PB5DIVbits.PBDIV = 1; // Peripheral Bus 5 Clock Divisor Control (PBCLK5 is SYSCLK divided by 2)
+
+    // PB7DIV
+    PB7DIVbits.ON = 1; // Peripheral Bus 7 Output Clock Enable (Output clock is enabled)
+    PB7DIVbits.PBDIV = 0; // Peripheral Bus 7 Clock Divisor Control (PBCLK7 is SYSCLK divided by 1)
+
+    // PB8DIV
+    PB8DIVbits.ON = 1; // Peripheral Bus 8 Output Clock Enable (Output clock is enabled)
+    PB8DIVbits.PBDIV = 1; // Peripheral Bus 8 Clock Divisor Control (PBCLK8 is SYSCLK divided by 2)
+
+    // PRECON - Set up prefetch
+    PRECONbits.PFMSECEN = 0; // Flash SEC Interrupt Enable (Do not generate an interrupt when the PFMSEC bit is set)
+    PRECONbits.PREFEN = 0b11; // Predictive Prefetch Enable (Enable predictive prefetch for any address)
+    PRECONbits.PFMWS = 0b010; // PFM Access Time Defined in Terms of SYSCLK Wait States (Two wait states)
+
+    // Set up caching
+    cp0 = _mfc0(16, 0);
+    cp0 &= ~0x07;
+    cp0 |= 0b011; // K0 = Cacheable, non-coherent, write-back, write allocate
+    _mtc0(16, 0, cp0);  
+
+    // Lock Sequence
+    SYSKEY = 0x33333333;
+    asm volatile("ei"); // Enable all interrupts
+}
+
+void delay_ms(int ms)
+{
+    ms *= (SYS_FREQ / 1000 / 2 / 32); // /2 due to PB_clock being DIV_2
+    
+    T8CON = 0;
+    T9CON = 0;
+    PR8 = (delay & 0xFFFF); // Low word in PR8
+    PR9 = (delay >> 16); // High word in PR9
+    T8CON = 0x8008;  // Bit 15 = timer enable, bit 3 = 32-bit mode on
+    
+    TMR8 = 0;
+    TMR9 = 0;
+    while (TMR9 < (delay >> 16)); // Delay ms milliseconds	
+}
+
+void main()
+{
+	// Set up all ports to be digital and not analog
+    ANSELA = 0;
+    ANSELB = 0;
+    ANSELC = 0;
+    ANSELD = 0;
+    ANSELE = 0;
+    ANSELF = 0;
+    ANSELG = 0;
+    ANSELH = 0;
+    ANSELJ = 0;
+	
+	// Set max performance mode
+	set_performance_mode();
+	
+    TRISACLR = 1 << 0; // Clear bit 0 of TRISA. which makes PORT A bit 0 an output
+
+    LATASET = 1 << 0; // Set bit 0 of PORT A to 1
+	
+	// Main loop
+	while (1)
+	{
+		// Insert code here
+	}
+}
